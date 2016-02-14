167007ccaee962467a32b09bde500e2760b30834
Ahmed Hassan <a.hassan@buismed.de>
2016-02-14 12:08:36 +0100

    update git url

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
     name: 'nerdmed:tinymce-helper',
-    version: '0.0.1',
+    version: '0.0.2',
 
     // Brief, one-line summary of the package.
     summary: 'Simple Block Helper for TinyMce in Blaze',
 
     // URL to the Git repository containing the source code for this package.
-    git: '',
+    git: 'git@github.com:nerdmed/tinymce-helper.git',
 
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
